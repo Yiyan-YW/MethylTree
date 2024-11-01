40a877023cb229589443d51c1b99daf845046f9c
ShouWenWang <wangsw09@gmail.com>
2024-11-01 19:32:25 +0800

    simplify settings

--- methyltree/settings.py
+++ methyltree/settings.py
@@ -1,26 +1,8 @@
 import os
 
-default_feature_dir = ''  # directory of selected genomic regions (bed format)
-source_script_dir = ''
-
-map_context = {"GC": "Acc", "CG": "Met"}
+#from envpath.settings import *
 
 help_function_dir, this_filename = os.path.split(__file__)
-
-#lambda-pUC19
-bismark_mm10_ref = (
-    "/storage/wangshouwenLab/shared_folder/reference/refdata-gex-mm10-lambda-pUC19/fasta"
-)
-bismark_hg38_ref = (
-    "/storage/wangshouwenLab/shared_folder/reference/refdata-gex-GRCh38-lambda-pUC19/fasta"
-)
-# transcriptome reference
-RNA_mm10_ref = "/storage/wangshouwenLab/shared_folder/reference/refdata-gex-mm10-2020-A"
-RNA_hg38_ref = (
-    "/storage/wangshouwenLab/shared_folder/reference/refdata-gex-GRCh38-2020-A"
-)
-
-CpG_ref_location='/storage/wangshouwenLab/shared_folder/reference/CpG'
 
 color_list = [
     "#1f78b4",
@@ -69,5 +51,7 @@ color_list_2 = [
     "#636363",
     "#ffd92f",
     "#b3b3b3",
+    "#db5f57",
+]
     "#db5f57",
 ]
